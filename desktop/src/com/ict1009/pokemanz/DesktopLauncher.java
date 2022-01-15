9f7a6ca34714bce3a3745593f64829e20306c9e6
Platy <daryl.limkz@gmail.com>
2022-01-15 18:59:26 +0800

    refactor(base): added background and player image
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- desktop/src/com/ict1009/pokemanz/DesktopLauncher.java
+++ desktop/src/com/ict1009/pokemanz/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.ict1009.pokemanz;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.ict1009.pokemanz.GameMain;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM
 // argument
@@ -10,6 +9,7 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
+        config.setWindowedMode(1080, 720);
         new Lwjgl3Application(new GameMain(), config);
     }
 }
