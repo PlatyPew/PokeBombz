2617431f9c5e845811da712c069aa54621f9e7a1
Platy <2101688@sit.singaporetech.edu.sg>
2022-01-31 15:35:19 +0800

    chore(bomb): added the constructor for the explosion
    Signed-off-by: Platy <2101688@sit.singaporetech.edu.sg>

--- core/src/com/ict1009/pokemanz/bomb/Explode.java
+++ core/src/com/ict1009/pokemanz/bomb/Explode.java
@@ -1,3 +1,20 @@
 package com.ict1009.pokemanz.bomb;
 
-public class Explode {}
+import com.ict1009.pokemanz.room.Map;
+import com.ict1009.pokemanz.room.Obstacle;
+
+public class Explode {
+    private Obstacle[][] obstacleMap;
+    private Bomb[][] bombMap;
+
+    private int gridX, gridY;
+    private int range;
+
+    public Explode(Map map, int gridX, int gridY, int range) {
+        this.obstacleMap = map.getObstacleMap();
+        this.bombMap = map.getBombMap();
+        this.gridX = gridX;
+        this.gridY = gridY;
+        this.range = range;
+    }
+}
